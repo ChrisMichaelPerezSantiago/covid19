9fbcbcc70e2ca1a57565577a7a47a3033b3a6a02
Chris Michael <chris.perez@upr.edu>
2020-03-19 11:48:27 -0400

    [Fatality Rate by AGE, Fatality Rate by SEX, Fatality Rate by COMORBIDITY] functions added to PluginManager.js

--- src/api/PluginManager.js
+++ src/api/PluginManager.js
@@ -36,7 +36,27 @@ class PluginManager {
   async getTestsInUS(){
     const res = await Promise.all(this.plugins.map(plugin => plugin.testsInUS()));
     return res;
-  }
+  };
+
+  async getFatalityRateByAge(){
+    const res = await Promise.all(this.plugins.map(plugin => plugin.fatalityRateByAge()));
+    return res;
+  };
+
+  async getFatalityRateBySex(){
+    const res = await Promise.all(this.plugins.map(plugin => plugin.fatalityRateBySex()));
+    return res;
+  };
+
+  async getFatalityRateByComorbidities(){
+    const res = await Promise.all(this.plugins.map(plugin => plugin.fatalityRateByComorbidities()));
+    return res;
+  };
+
+  async getCountriesWhereCoronavirusHasSpread(){
+    const res = await Promise.all(this.plugins.map(plugin => plugin.countriesWhereCoronavirusHasSpread()));
+    return res;
+  };
 }
 
 module.exports = PluginManager;
