b64ad03d22876ba1df5eb63b3d12287104080e3c
Chris Michael <chris.perez@upr.edu>
2020-04-10 14:48:12 -0400

    requests helper function implemented and added in the utils.js

--- server/src/api/utils/utils.js
+++ server/src/api/utils/utils.js
@@ -1,3 +1,5 @@
+const axios = require('axios').default;
+
 const renameKey = (obj, old_key, new_key) => {
   if (old_key !== new_key) {
     Object.defineProperty(obj, new_key,
@@ -6,6 +8,23 @@ const renameKey = (obj, old_key, new_key) => {
   }
 };
 
+const requests = (url) =>{
+  const options ={
+    method: 'GET',
+    responseType: 'json'
+  }
+  return new Promise((resolve , reject) =>{
+    axios.get(url , {options})
+      .then(res =>{
+        const data = res.data;
+        return data;
+      }).then(json =>{
+        resolve(json)
+      })
+  })
+}
+
 module.exports = {
-  renameKey
+  renameKey,
+  requests
 };
